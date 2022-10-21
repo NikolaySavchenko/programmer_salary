8eb105d7498c0c6e9285b6a3d954381eea92f9f4
Nikolay Savchenko <7548568@gmail.com>
2022-10-21 22:50:48 +0300

    Fixed comparisons with None, 0

--- utils.py
+++ utils.py
@@ -17,38 +17,37 @@ def get_vacancies_hh(job_name, number_of_pages, par):
 
 
 def predict_salary(salary_from, salary_to):
-    if (salary_from is not None and salary_to is not None and
-            salary_from != 0 and salary_to != 0):
+    if salary_from and salary_to:
         return ((int(salary_from) + int(salary_to))) / 2
-    elif (salary_from is None or salary_from == 0) and (salary_to is not None
-                                                        and salary_to != 0):
+    elif not salary_from and salary_to:
         return int(salary_to) * 0.8
-    elif (salary_from is not None and salary_from != 0) and (salary_to is None or
-                                                             salary_to != 0):
+    elif salary_from and not salary_to:
         return int(salary_from) * 1.2
     else:
         return
 
 
 def predict_rub_salary_hh(vacancy):
-    if vacancy["salary"] is None:
+    if not vacancy["salary"]:
         return
     elif vacancy["salary"]["currency"] != 'RUR':
         return
     else:
-        return predict_salary(vacancy["salary"]["from"], vacancy["salary"]["to"])
+        return predict_salary(vacancy["salary"]["from"],
+                              vacancy["salary"]["to"])
 
 
 def predict_rub_salary_sj(vacancy):
-    if vacancy["currency"] != 'rub' or vacancy["currency"] is None:
+    if vacancy["currency"] != 'rub' or not vacancy["currency"]:
         return
     else:
-        return predict_salary(vacancy['payment_from'], vacancy['payment_to'])
+        return predict_salary(vacancy['payment_from'],
+                              vacancy['payment_to'])
 
 
 def transformation_for_table(statistic):
     for_table = [['Язык программирования', 'Вакансий найдено',
-                   'Вакансий обработано', 'Средняя зарплата'], ]
+                  'Вакансий обработано', 'Средняя зарплата'], ]
     for language in statistic:
         language_solar = [language, statistic[language]['vacancies_found'],
                           statistic[language]['vacancies_processed'],
